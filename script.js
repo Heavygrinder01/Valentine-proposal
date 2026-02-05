f1a1d7279be36ca7d8264aa7037359c5229ad59a
Heavygrinder01 <62474313+Heavygrinder01@users.noreply.github.com>
2026-02-05 23:51:01 +0530

    script.js

--- script.js
+++ script.js
@@ -1,84 +1,89 @@
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+document.body.appendChild(tag);
 
 var player;
 var isYouTubeReady = false;
 
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('youtube-player', {
-    height: '100%',
-    width: '100%',
-    videoId: '09R8_2nJtjg',
+  player = new YT.Player("youtube-player", {
+    videoId: "09R8_2nJtjg",
     playerVars: {
       start: 40,
-      controls: 0,
       autoplay: 1,
       mute: 1,
+      controls: 0,
       rel: 0
     },
     events: {
-      onReady: onPlayerReady
+      onReady: function () {
+        isYouTubeReady = true;
+      }
     }
   });
 }
 
-function onPlayerReady() {
-  isYouTubeReady = true;
-}
-
 function showMessage(response) {
-  let videoPlayed = false;
-
   if (response === "No") {
     const noButton = document.getElementById("no-button");
-    const maxWidth = window.innerWidth - noButton.offsetWidth;
-    const maxHeight = window.innerHeight - noButton.offsetHeight;
+    const card = document.querySelector(".card");
 
-    noButton.style.position = "absolute";
     document.getElementsByClassName("image")[0].src = "images/gun.gif";
-
     document.getElementById("question").textContent = "Choose wisely";
     document.getElementById("name").style.display = "none";
 
-    noButton.addEventListener("mouseover", () => {
-      if (!videoPlayed && isYouTubeReady && player) {
+    noButton.style.position = "absolute";
+
+    noButton.addEventListener("touchstart", moveButton);
+    noButton.addEventListener("mouseover", moveButton);
+
+    function moveButton() {
+      const cardRect = card.getBoundingClientRect();
+      const btnRect = noButton.getBoundingClientRect();
+
+      const maxX = cardRect.width - btnRect.width;
+      const maxY = cardRect.height - btnRect.height;
+
+      const randomX = Math.random() * maxX;
+      const randomY = Math.random() * maxY;
+
+      noButton.style.left = randomX + "px";
+      noButton.style.top = randomY + "px";
+
+      if (isYouTubeReady && player) {
         player.unMute();
         player.playVideo();
-        videoPlayed = true;
       }
-
-      noButton.style.left = Math.floor(Math.random() * maxWidth) + "px";
-      noButton.style.top = Math.floor(Math.random() * maxHeight) + "px";
-    });
+    }
   }
 
   if (response === "Yes") {
     document.getElementById("name").remove();
     document.getElementById("no-button").remove();
+    document.getElementById("yesButton").remove();
 
-    const playerContainer = document.getElementById("youtube-player-container");
-    if (playerContainer) playerContainer.remove();
+    document.getElementById("question").textContent =
+      "See you on the 14th my princess";
+
+    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+
+    document.getElementById("moreButton").style.display = "block";
+
+    const audio = document.createElement("audio");
+    audio.src = "./Minions Cheering.mp4";
+    audio.play();
+
+    const yt = document.getElementById("youtube-player-container");
+    if (yt) yt.remove();
 
     if (player) {
       player.stopVideo();
       player.destroy();
     }
-
-    const audioElement = document.createElement("audio");
-    audioElement.src = "./Minions Cheering.mp4";
-    audioElement.play();
-
-    document.getElementById("question").textContent = "See you on the 14th my princess";
-    document.getElementsByClassName("image")[0].src = "images/dance.gif";
-
-    document.getElementById("yesButton").remove();
-    document.getElementById("moreButton").style.display = "inline-block";
   }
 }
 
 function showLetter() {
-  document.getElementById("moreButton").remove();
+  document.getElementById("moreButton").style.display = "none";
   document.getElementById("letter").style.display = "block";
 }
